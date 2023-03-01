73dfcd39170d96de4e44e923e6ad74d463f37c27
David <rrazodavid@gmail.com>
2023-03-01 15:27:15 -0800

    bug fix 1

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = ({ products, bannerData }) => (
     <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
     <div className="products-heading">
       <h2>Best Seller Products</h2>
-      <p>See what's popular!</p>
+      <p>popular!</p>
     </div>
 
     <div className="products-container">
